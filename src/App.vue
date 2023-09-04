40f99765a21e32e02ffbcd3169d687b7e73db94e
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-09-04 11:49:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterView></RouterView>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  test
+</template>
+
+<script>
+import axios from 'axios';
+import { store } from '../store';
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      store,
+      announcement: null, // Inizializziamo announcement come null
+    }
+  },
+  methods: {
+    async getData() {
+      try {
+        const response = await axios.get(`${store.apiUrl}/home`)
+        this.annoucement = response.data.result;
+        console.log(this.annoucement);
+      } catch (error) {
+        console.error('Errore nella chiamata API:', error);
+      }
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+
+}
+</script>
+
+<style lang="scss" scoped></style>
+
+<!-- blue-1= #4383e2; -->
